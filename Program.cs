20d001cb43191130ebcb2cd68b17cd1f8740eedf
Jhon Anthony Valenzuela Armas <N00471400@upn.pe>
2026-06-18 02:40:12 -0500

    Inicializacion de variables de personaje

--- Program.cs
+++ Program.cs
@@ -1,1 +1,28 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+namespace T3Game
+{
+    class Program
+    {
+        //Variables globales del personaje principal
+        static int jugadorNivel=1;
+        static int jugadorHPMax=100;
+        static int jugadorHPActual=100;
+        static int jugadorManaMax=50;
+        static int jugadorManaACtual=50;
+        static int jugadorAtaque=15;
+        static int jugadorDefensa=5;
+        static int JugadorVelocidad=10;
+        static int jugadorOro=0;
+        static int jugadorEXP=0;
+
+        //Posición del jugador
+        static int jugadorFila=1;
+        static int jugadorColumna=1;
+        
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Personaje creado satisfactoriamente-");
+            System.Console.WriteLine($"Nivel: {jugadorNivel} | HP: {jugadorHPActual}");
+        }
+    }
+}
